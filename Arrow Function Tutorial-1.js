790fc66080cf4baafe03acd3f486d3ad0cfc877a
Alex Zuniga <rthe3rd.z@gmail.com>
2022-10-10 09:00:13 -0500

    Initial commit

---
+++ Arrow Function Tutorial-1.js
@@ -0,0 +1,23 @@
+class Person{
+    constructor(name) {
+        this.name = name
+    
+}
+
+printNameArrow(){
+    setTimeout(() => {
+        console.log('Arraw: ' + this.name)
+    
+    }, 100)
+}
+
+printNameFunction(){
+    setTimeout(function() {
+        console.log('Function: ' + this.name)
+    }, 100)
+}
+}
+
+let person = new Person('Bob')
+person.printNameArrow()
+person.printNameFunction()
